a56219cad360d48185402a365e9f4fe91bfa18a3
Svavushin <shavushin@gmail.com>
2023-11-07 17:25:34 +0900

    f

--- php/db.php
+++ php/db.php
@@ -1,4 +1,13 @@
 <?php
 $servername = "localhost";
 $_username = "root";
-$_password = "";
+$_password = "100205";
+
+//коннект к бд
+$connect_sql = new mysqli($servername, $_username, $_password);
+
+
+$db_users = $connect_sql->select_db('auth');
+
+
+$msg_logout = $_COOKIE['log_msg'];
--- php/example.php
+++ php/example.php
@@ -2,14 +2,6 @@
 include 'db.php';
 
 
-//коннект к бд
-$connect_sql = new mysqli($servername, $_username, $_password);
-
-
-$db_users = $connect_sql->select_db('auth');
-
-
-$msg_logout = $_COOKIE['log_msg'];
 
 
 if ($_COOKIE['username'] == '1234'){
@@ -103,8 +95,6 @@ if (($_SERVER["REQUEST_METHOD"] == "POST") && ($connect_sql)) {
 
 
 
-
-!DOCTYPE html;
 
 <html>
 
